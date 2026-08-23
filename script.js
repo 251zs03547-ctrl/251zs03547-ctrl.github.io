517195bcbf399a548e6aab60bb62cfa64e69bdfc
251zs03547-ctrl <251zs03547@student.zen.ac.jp>
2026-08-23 16:02:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 
 addButton.addEventListener("click", function () {
+
   const task = taskInput.value;
 
-  if (task === "") {
+  if (task.trim() === "") {
     alert("やることを入力してください");
     return;
   }
@@ -13,14 +14,11 @@ addButton.addEventListener("click", function () {
   const li = document.createElement("li");
   li.textContent = task;
 
-  // クリックすると完了状態にする
   li.addEventListener("click", function () {
     li.classList.toggle("completed");
   });
 
-  // タスクを追加
   taskList.appendChild(li);
 
-  // 入力欄を空にする
   taskInput.value = "";
 });
